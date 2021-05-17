d728e9108c4cd33a09affb1cbec62691957e6257
David Matiashvili <70051407+dav26072000@users.noreply.github.com>
2021-05-17 14:27:21 +0400

    build(all doc) end task2

---
+++ task2.js
@@ -0,0 +1,17 @@
+const obj = {
+  _name: [], // ['name', length][]
+  set name(val) {
+    val = val.split(", ");
+    let newArr = [];
+    for (const element in val) {
+      newArr[element] = [val[element], val[element].length];
+    }
+    this._name = newArr;
+  },
+  get name() {
+    return this._name;
+  },
+};
+
+obj.name = "Vrezh, Artavazd";
+// console.log(obj.name); // [['Vrezh', 7], ['Artavazd',10]]
